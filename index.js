d06533947f5471d9c7bd493c40423eb57a4b2ec8
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-02-18 22:52:15 +1100

    Generate assets using async template functions

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
+var Promise = require('bluebird');
+var map = require('lodash.map');
+
 function FileWebpackPlugin(options) {
   this.options = options || {};
 }
 
 FileWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
-  compiler.plugin('emit', function(compiler, callback) {
-    Object.keys(self.options).forEach(function(filename) {
-      var helloWorld = 'Hello World!';
-      compiler.assets[filename] = {
-        source: function() {
-          return helloWorld;
-        },
-        size: function() {
-          return helloWorld.length;
-        }
-      };
+  compiler.plugin('emit', function(compiler, done) {
+    var data = {};
+
+    var promises = map(self.options, function(asyncTemplate, filename) {
+      return new Promise(function(reject, resolve) {
+        asyncTemplate(data, function(err, contents) {
+          if (err) return reject(err);
+
+          compiler.assets[filename] = createAsset(contents);
+
+          resolve();
+        });
+      });
     });
 
-    callback();
+    Promise.all(promises)
+      .then(function() {
+        done();
+      }, function(err) {
+        done(err);
+      });
   });
 };
+
+function createAsset(contents) {
+  return {
+    source: function() {
+      return contents;
+    },
+    size: function() {
+      return contents.length;
+    }
+  };
+}
 
 module.exports = FileWebpackPlugin;
